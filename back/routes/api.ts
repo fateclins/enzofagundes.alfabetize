369cee3ada1ef78d86ab189611d8d99e5355317b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-17 17:11:11 -0300

    chore: Add route for root endpoint

--- back/routes/api.ts
+++ back/routes/api.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./user.routes";
 
 const routes = Router();
+routes.get("/", (req, res) => res.status(200).json({ message: "Hello, world!" }));
 userRoutes(routes);
 
 export default routes;
