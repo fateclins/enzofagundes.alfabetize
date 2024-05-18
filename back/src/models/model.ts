8e2ef3e6d57f82ee74b4639b45fe8ca348e2b0bf
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-18 09:25:55 -0300

    feat: Refactor Model class to use schema and table variables

--- back/src/models/model.ts
+++ back/src/models/model.ts
@@ -1,6 +1,6 @@
 import type { PgTable } from "drizzle-orm/pg-core";
 import { db, schema } from "../../db/db";
-import type { TableNames } from "../../db/schema";
+import type { SchemaNames } from "../../db/schema";
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import { eq } from "drizzle-orm";
 
@@ -8,37 +8,45 @@ class Model
 {
     protected fillable: string[];
     protected db: PostgresJsDatabase = db;
-    protected table: PgTable<any>;
+    protected schema: PgTable<any>;
+    protected table: string;
 
-    constructor(tableName: TableNames)
+    constructor(tableName: SchemaNames, table: string)
     {
         this.fillable = [];
-        this.table = schema[tableName];
+        this.schema = schema[tableName];
+        this.table = table;
     }
 
     public async create(data: any): Promise<any>
     {
-        return await this.db.insert(this.table).values(data).returning();
+        return await this.db.insert(this.schema).values(data).returning();
     }
 
     public async find(id: number): Promise<any>
     {
-        return await this.db.select().from(this.table).where(eq((this.table as any).id, id));
+        return await this.db.select().from(this.schema).where(eq((this.schema as any).id, id));
     }
 
     public async all(): Promise<any>
     {
-        return await this.db.select().from(this.table);
+        return await this.db.select().from(this.schema);
     }
 
     public async destroy(id: number): Promise<any>
     {
-        return await this.db.delete(this.table).where(eq((this.table as any).id, id));
+        return await this.db.delete(this.schema).where(eq((this.schema as any).id, id));
     }
 
     public async update(id: number, data: any): Promise<any>
     {
-        return await this.db.update(this.table).set(data).where(eq((this.table as any).id, id));
+        return await this.db.update(this.schema).set(data).where(eq((this.schema as any).id, id));
+    }
+
+    public async leftJoin(table: string, column: string, condition: string): Promise<any>
+    {
+        const tableSchema = schema[table as SchemaNames];
+        return await this.db.select().from(this.schema).leftJoin(tableSchema, eq((this.schema as any)[column], condition));
     }
 }
 
