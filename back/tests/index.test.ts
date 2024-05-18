4a825bb9811273a19ea15511b49f18ce5b4c5ffc
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-18 14:32:59 -0300

    feat: Add test for index route
    This commit adds a test for the index route in the `index.test.ts` file. The test ensures that the route returns a status code of 200 and the expected response body. This change enhances the test coverage and verifies the functionality of the index route.

---
+++ back/tests/index.test.ts
@@ -0,0 +1,11 @@
+import { expect, describe, test } from "bun:test";
+import request from "supertest";
+import { app } from "../src/server";
+
+describe("Testing the index file", () => {
+    test("Should be return Hello World", async () => {
+        const response = await request(app).get("/api");
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: "Hello, world!" });
+    });
+});
