c1950c4b41cafa77f9f06c3596fd31ee7253a7ea
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-23 11:13:37 -0300

    ```text feat: Update User entity to include a table name
    This commit updates the User entity in the local data sources package. It adds a table name annotation to specify the name of the table in the database. This change ensures that the User entity is correctly mapped to the "users" table in the database, allowing for proper data retrieval and manipulation.

--- front/app/src/main/java/com/example/alfabetize/data/datasources/local/entities/User.kt
+++ front/app/src/main/java/com/example/alfabetize/data/datasources/local/entities/User.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "users")
 data class User(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "email") val email: String?,
