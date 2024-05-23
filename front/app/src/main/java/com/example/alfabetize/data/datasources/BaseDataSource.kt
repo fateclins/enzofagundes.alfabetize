63dde8ca6c75d23798b3b60ee28861addec637d5
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-23 09:30:30 -0300

    Refactor package structure of domain models

--- front/app/src/main/java/com/example/alfabetize/data/datasources/BaseDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alfabetize.data.datasources
-
-interface BaseDataSource {
-    public fun index();
-    public fun show();
-    public fun create();
-    public fun update();
-    public fun delete();
-}
