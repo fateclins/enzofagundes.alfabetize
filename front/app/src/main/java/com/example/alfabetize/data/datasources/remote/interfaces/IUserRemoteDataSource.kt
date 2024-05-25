75731c6ea0f143fcfb1ebb3f51356ddf0caa3178
enzofagundz <enzo.fagundes014@gmail.com>
2024-05-25 17:26:08 -0300

    feat: Add remote data source interfaces for ActivityExercises, Activity, Exercise, Module, Profile, and User

--- front/app/src/main/java/com/example/alfabetize/data/datasources/remote/interfaces/IUserRemoteDataSource.kt
+++ front/app/src/main/java/com/example/alfabetize/data/datasources/remote/interfaces/IUserRemoteDataSource.kt
@@ -1,9 +1,11 @@
 package com.example.alfabetize.data.datasources.remote.interfaces
 
+import com.example.alfabetize.data.datasources.remote.api.ApiService
 import com.example.alfabetize.data.datasources.remote.entities.User
 import retrofit2.Call
 
 interface IUserRemoteDataSource {
+    val apiService: ApiService
     suspend fun getAllUsers(): Call<List<User>>
     suspend fun getUser(id: Int): Call<User>
     suspend fun insertUser(user: User): Call<User>
